4216b724e109d61594949634f57f44ae4921a6c4
Denis <denisurgel04@gmail.com>
2025-04-28 16:33:41 +0200

    ajout de la page entreprise

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 let menuAccueil = document.getElementById('accueil_menu');
 let menuApropos = document.getElementById('a_propos_menu');
+let menuEcoleEntreprise = document.getElementById('ecole_entreprise_menu');
 let menuProjets = document.getElementById('projets_menu');
 let menuVeille = document.getElementById('veille_menu');
 let menuContact = document.getElementById('contact_menu');
@@ -7,6 +8,7 @@ let menuRealisation = document.getElementById('realisation_menu');
 
 let lienAccueil = document.getElementById('lien_accueil_menu');
 let lienApropos = document.getElementById('lien_a_propos_menu');
+let lienEcoleEntreprise = document.getElementById('lien_ecole_entreprise_menu');
 let lienProjets = document.getElementById('lien_projets_menu');
 let lienVeille = document.getElementById('lien_veille_menu');
 let lienContact = document.getElementById('lien_contact_menu');
@@ -26,6 +28,7 @@ burgerMobile.addEventListener('click', () => {
         menuMobile.style.height = "0";
         menuAccueil.style.height = "0";
         menuApropos.style.height = "0";
+        menuEcoleEntreprise.style.height = "0";
         menuProjets.style.height = "0";
         menuVeille.style.height = "0";
         menuContact.style.height = "0";
@@ -34,6 +37,7 @@ burgerMobile.addEventListener('click', () => {
         menuMobile.style.fontSize = "0";
         menuAccueil.style.fontSize = "0";
         menuApropos.style.fontSize = "0";
+        menuEcoleEntreprise.style.fontSize = "0";
         menuProjets.style.fontSize = "0";
         menuVeille.style.fontSize = "0";
         menuContact.style.fontSize = "0";
@@ -41,6 +45,7 @@ burgerMobile.addEventListener('click', () => {
 
         lienAccueil.style.padding = "0";
         lienApropos.style.padding = "0";
+        lienEcoleEntreprise.style.padding = "0";
         lienProjets.style.padding = "0";
         lienVeille.style.padding = "0";
         lienContact.style.padding = "0";
@@ -51,6 +56,7 @@ burgerMobile.addEventListener('click', () => {
         menuMobile.style.height = "100%";
         menuAccueil.style.height = "100%";
         menuApropos.style.height = "100%";
+        menuEcoleEntreprise.style.height = "100%";
         menuProjets.style.height = "100%";
         menuVeille.style.height = "100%";
         menuContact.style.height = "100%";
@@ -59,6 +65,7 @@ burgerMobile.addEventListener('click', () => {
         menuMobile.style.fontSize = "24px";
         menuAccueil.style.fontSize = "24px";
         menuApropos.style.fontSize = "24px";
+        menuEcoleEntreprise.style.fontSize = "24px";
         menuProjets.style.fontSize = "24px";
         menuVeille.style.fontSize = "24px";
         menuContact.style.fontSize = "24px";
@@ -67,6 +74,7 @@ burgerMobile.addEventListener('click', () => {
 
         lienAccueil.style.padding = "10px";
         lienApropos.style.padding = "10px";
+        lienEcoleEntreprise.style.padding = "10px";
         lienProjets.style.padding = "10px";
         lienVeille.style.padding = "10px";
         lienContact.style.padding = "10px";
@@ -83,6 +91,9 @@ burgerMobile.addEventListener('click', () => {
             break;
         case 'À Propos':
             lienApropos.style.display = "none";
+            break;
+        case 'École et Entreprise':
+            lienEcoleEntreprise.style.display = "none";
             break;
         case 'Réalisations et Missions':
             lienRealisation.style.display = "none";
